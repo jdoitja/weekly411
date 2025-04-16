a72b96ef832774f5f8e1f605b584cee7d70fc2df
j <andcl.island.12@gmail.com>
2025-04-16 14:54:42 +0900

    weekly

--- src/main/java/com/example/weekly411/Weekly411Application.java
+++ src/main/java/com/example/weekly411/Weekly411Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Weekly411Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Weekly411Application.class, args);
 	}
